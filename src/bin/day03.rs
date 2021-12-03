7dfbfb14fda7f12549b0c709465563cd83f4fd8f
Jon Chambers <jon.chambers@gmail.com>
2021-12-03 10:14:33 -0500

    Add a solution for day 3, part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,6 +1,6 @@
-use std::{env, error, io};
 use std::fs::File;
 use std::io::BufRead;
+use std::{env, error, io};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -8,11 +8,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     if let Some(path) = args.get(1) {
         let file = File::open(path)?;
 
-        let values: Vec<String> = io::BufReader::new(file).lines()
+        let values: Vec<String> = io::BufReader::new(file)
+            .lines()
             .map(|line| line.unwrap())
             .collect();
 
         println!("Power consumption: {}", get_power_consumption(&values));
+        println!("Life support rating: {}", get_life_support_rating(&values));
 
         Ok(())
     } else {
@@ -59,6 +61,55 @@ fn get_gamma(values: &[String]) -> u32 {
     })
 }
 
+fn get_life_support_rating(values: &[String]) -> u32 {
+    get_oxygen_generator_rating(values) * get_co2_scrubber_rating(values)
+}
+
+fn get_oxygen_generator_rating(values: &[String]) -> u32 {
+    get_life_support_value(values, 0, |ones_at_position, len| {
+        if ones_at_position > len / 2 || (len % 2 == 0 && ones_at_position == len / 2) {
+            '1'
+        } else {
+            '0'
+        }
+    })
+}
+
+fn get_co2_scrubber_rating(values: &[String]) -> u32 {
+    get_life_support_value(values, 0, |ones_at_position, len| {
+        if ones_at_position > len / 2 || (len % 2 == 0 && ones_at_position == len / 2) {
+            '0'
+        } else {
+            '1'
+        }
+    })
+}
+
+fn get_life_support_value(
+    values: &[String],
+    position: usize,
+    get_target_digit: impl Fn(usize, usize) -> char,
+) -> u32 {
+    if values.len() > 1 {
+        let ones_at_position = values
+            .iter()
+            .filter(|value| value.chars().nth(position) == Some('1'))
+            .count();
+
+        let target_digit = get_target_digit(ones_at_position, values.len());
+
+        let filtered_values: Vec<String> = values
+            .iter()
+            .filter(|value| value.chars().nth(position) == Some(target_digit))
+            .map(|value| value.clone())
+            .collect();
+
+        get_life_support_value(&filtered_values, position + 1, get_target_digit)
+    } else {
+        u32::from_str_radix(values[0].as_str(), 2).unwrap()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -90,5 +141,26 @@ mod test {
         let values: Vec<String> = VALUES.iter().map(|v| String::from(*v)).collect();
 
         assert_eq!(198, get_power_consumption(&values));
+    }
+
+    #[test]
+    fn test_get_oxygen_generator_rating() {
+        let values: Vec<String> = VALUES.iter().map(|v| String::from(*v)).collect();
+
+        assert_eq!(23, get_oxygen_generator_rating(&values));
+    }
+
+    #[test]
+    fn test_get_co2_scrubber_rating() {
+        let values: Vec<String> = VALUES.iter().map(|v| String::from(*v)).collect();
+
+        assert_eq!(10, get_co2_scrubber_rating(&values));
+    }
+
+    #[test]
+    fn test_get_life_support_rating() {
+        let values: Vec<String> = VALUES.iter().map(|v| String::from(*v)).collect();
+
+        assert_eq!(230, get_life_support_rating(&values));
     }
 }

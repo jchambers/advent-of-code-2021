1c6710ee092d6b796a40a82788afb7e6e176c9ef
Jon Chambers <jon.chambers@gmail.com>
2021-12-10 12:05:33 -0500

    Add a solution for day 10, part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -8,11 +8,22 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     if let Some(path) = args.get(1) {
-        let total_score = io::BufReader::new(File::open(path)?).lines()
-            .map(|line| syntax_score(line.unwrap().as_str()))
-            .sum::<u32>();
+        println!(
+            "Total syntax score: {}",
+            io::BufReader::new(File::open(path)?)
+                .lines()
+                .map(|line| syntax_score(line.unwrap().as_str()))
+                .sum::<u32>()
+        );
 
-        println!("Total syntax score: {}", total_score);
+        println!(
+            "Median autocomplete score: {}",
+            median_autocomplete_score(
+                io::BufReader::new(File::open(path)?)
+                    .lines()
+                    .map(|line| line.unwrap())
+            )
+        );
 
         Ok(())
     } else {
@@ -22,7 +33,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
 #[derive(Debug, Eq, PartialEq)]
 enum NavigationSyntaxError {
-    Incomplete { expected: char },
+    Incomplete { expected: String },
     Invalid { expected: char, found: char },
 }
 
@@ -48,10 +59,14 @@ fn analyze_line(line: &str) -> Result<(), NavigationSyntaxError> {
         };
     }
 
-    if let Some(c) = stack.front() {
-        Err(Incomplete {
-            expected: expected_closer(c),
-        })
+    if let Some(_) = stack.front() {
+        let mut expected = String::new();
+
+        while let Some(opener) = stack.pop_front() {
+            expected.push(expected_closer(&opener));
+        }
+
+        Err(Incomplete { expected })
     } else {
         Ok(())
     }
@@ -78,18 +93,52 @@ fn expected_opener(closer: &char) -> char {
 }
 
 fn syntax_score(line: &str) -> u32 {
-    match analyze_line(line) {
-        Err(Invalid { expected: _, found: c }) => {
-            match c {
-                ')' => 3,
-                ']' => 57,
-                '}' => 1197,
-                '>' => 25137,
-                _ => unreachable!()
-            }
-        },
-        _ => 0
+    if let Err(Invalid {
+        expected: _,
+        found: c,
+    }) = analyze_line(line)
+    {
+        match c {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => unreachable!(),
+        }
+    } else {
+        0
     }
+}
+
+fn autocomplete_score(line: &str) -> u64 {
+    if let Err(Incomplete {
+        expected: completion,
+    }) = analyze_line(line)
+    {
+        completion
+            .chars()
+            .map(|c| match c {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => unreachable!(),
+            })
+            .fold(0, |total, char_score| (total * 5) + char_score)
+    } else {
+        0
+    }
+}
+
+fn median_autocomplete_score(lines: impl Iterator<Item = String>) -> u64 {
+    let mut scores: Vec<u64> = lines
+        .map(|line| autocomplete_score(line.as_str()))
+        .filter(|&score| score > 0)
+        .collect();
+
+    scores.sort();
+
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -151,12 +200,68 @@ mod test {
             }),
             analyze_line("<([]){()}[{}])")
         );
+
+        assert_eq!(
+            Err(Incomplete {
+                expected: String::from("}}]])})]")
+            }),
+            analyze_line("[({(<(())[]>[[{[]{<()<>>")
+        );
+
+        assert_eq!(
+            Err(Incomplete {
+                expected: String::from(")}>]})")
+            }),
+            analyze_line("[(()[<>])]({[<{<<[]>>(")
+        );
+
+        assert_eq!(
+            Err(Incomplete {
+                expected: String::from("}}>}>))))")
+            }),
+            analyze_line("(((({<>}<{<{<>}{[]{[]{}")
+        );
+
+        assert_eq!(
+            Err(Incomplete {
+                expected: String::from("]]}}]}]}>")
+            }),
+            analyze_line("{<[[]]>}<{[{[{[]{()[[[]")
+        );
+
+        assert_eq!(
+            Err(Incomplete {
+                expected: String::from("])}>")
+            }),
+            analyze_line("<{([{{}}[<[[[<>{}]]]>[]]")
+        );
     }
 
     #[test]
     fn test_syntax_score() {
-        assert_eq!(26397, TEST_INPUT.lines()
-            .map(|line| syntax_score(line))
-            .sum::<u32>());
+        assert_eq!(
+            26397,
+            TEST_INPUT
+                .lines()
+                .map(|line| syntax_score(line))
+                .sum::<u32>()
+        );
+    }
+
+    #[test]
+    fn test_autocomplete_score() {
+        assert_eq!(288957, autocomplete_score("[({(<(())[]>[[{[]{<()<>>"));
+        assert_eq!(5566, autocomplete_score("[(()[<>])]({[<{<<[]>>("));
+        assert_eq!(1480781, autocomplete_score("(((({<>}<{<{<>}{[]{[]{}"));
+        assert_eq!(995444, autocomplete_score("{<[[]]>}<{[{[{[]{()[[[]"));
+        assert_eq!(294, autocomplete_score("<{([{{}}[<[[[<>{}]]]>[]]"));
+    }
+
+    #[test]
+    fn test_median_autocomplete_score() {
+        assert_eq!(
+            288957,
+            median_autocomplete_score(TEST_INPUT.lines().map(|line| String::from(line)))
+        );
     }
 }

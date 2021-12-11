38b0e536ffc6d51bb25d0bebf15e5be628fcec76
Jon Chambers <jon.chambers@gmail.com>
2021-12-11 01:05:31 -0500

    Clean up formatting, tests, and encapsulation

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,38 +1,22 @@
+use std::collections::HashSet;
 use std::str::FromStr;
 use std::{env, error};
-use std::collections::HashSet;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     if let Some(path) = args.get(1) {
-        {
-            let mut grid = OctopusGrid::from_str(std::fs::read_to_string(path)?.as_str())?;
+        println!(
+            "Flashes after 100 steps: {}",
+            OctopusGrid::from_str(std::fs::read_to_string(path)?.as_str())?
+                .flashes_after_steps(100)
+        );
 
-            let mut flashes = 0;
-
-            for _ in 0..100 {
-                flashes += grid.advance();
-            }
-
-            println!("Flashes after 100 steps: {}", flashes);
-        }
-
-        {
-            let mut grid = OctopusGrid::from_str(std::fs::read_to_string(path)?.as_str())?;
-
-            let mut step = 0;
-
-            loop {
-                step += 1;
-
-                if grid.advance() == 100 {
-                    break;
-                }
-            }
-
-            println!("Synchronization on step: {}", step);
-        }
+        println!(
+            "Steps until synchronization: {}",
+            OctopusGrid::from_str(std::fs::read_to_string(path)?.as_str())?
+                .steps_until_synchronization()
+        );
 
         Ok(())
     } else {
@@ -46,7 +30,29 @@ struct OctopusGrid {
 }
 
 impl OctopusGrid {
-    pub fn advance(&mut self) -> u32 {
+    pub fn flashes_after_steps(mut self, steps: u32) -> u32 {
+        let mut flashes = 0;
+
+        for _ in 0..steps {
+            flashes += self.advance();
+        }
+
+        flashes
+    }
+
+    pub fn steps_until_synchronization(mut self) -> u32 {
+        let mut step = 0;
+
+        loop {
+            step += 1;
+
+            if self.advance() == 100 {
+                break step;
+            }
+        }
+    }
+
+    fn advance(&mut self) -> u32 {
         for row in 0..10 {
             for col in 0..10 {
                 self.energy_levels[row][col] += 1;
@@ -93,29 +99,10 @@ impl OctopusGrid {
     fn neighbors(row: usize, col: usize) -> Vec<(usize, usize)> {
         let mut neighbors = Vec::new();
 
-        let min_row = if row > 0 {
-            row - 1
-        } else {
-            row
-        };
-
-        let max_row = if row < 9 {
-            row + 1
-        } else {
-            row
-        };
-
-        let min_col = if col > 0 {
-            col - 1
-        } else {
-            col
-        };
-
-        let max_col = if col < 9 {
-            col + 1
-        } else {
-            col
-        };
+        let min_row = if row > 0 { row - 1 } else { row };
+        let max_row = if row < 9 { row + 1 } else { row };
+        let min_col = if col > 0 { col - 1 } else { col };
+        let max_col = if col < 9 { col + 1 } else { col };
 
         for neighbor_row in min_row..=max_row {
             for neighbor_col in min_col..=max_col {
@@ -152,24 +139,24 @@ impl FromStr for OctopusGrid {
 
 #[cfg(test)]
 mod test {
-    use indoc::indoc;
     use super::*;
+    use indoc::indoc;
+
+    const TEST_GRID_STRING: &str = indoc! {"
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
+    "};
 
     #[test]
     fn test_octopus_grid_from_string() {
-        let grid_string = indoc!{"
-            5483143223
-            2745854711
-            5264556173
-            6141336146
-            6357385478
-            4167524645
-            2176841721
-            6882881134
-            4846848554
-            5283751526
-        "};
-
         let expected = OctopusGrid {
             energy_levels: [
                 [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
@@ -185,78 +172,69 @@ mod test {
             ],
         };
 
-        assert_eq!(expected, OctopusGrid::from_str(grid_string).unwrap());
+        assert_eq!(expected, OctopusGrid::from_str(TEST_GRID_STRING).unwrap());
     }
 
     #[test]
     fn test_advance() {
-        let mut grid = OctopusGrid::from_str(indoc! {"
-            5483143223
-            2745854711
-            5264556173
-            6141336146
-            6357385478
-            4167524645
-            2176841721
-            6882881134
-            4846848554
-            5283751526
-        "}).unwrap();
-
-        let after_first_step = OctopusGrid::from_str(indoc! {"
-            6594254334
-            3856965822
-            6375667284
-            7252447257
-            7468496589
-            5278635756
-            3287952832
-            7993992245
-            5957959665
-            6394862637
-        "}).unwrap();
-
-        let after_second_step = OctopusGrid::from_str(indoc! {"
-            8807476555
-            5089087054
-            8597889608
-            8485769600
-            8700908800
-            6600088989
-            6800005943
-            0000007456
-            9000000876
-            8700006848
-        "}).unwrap();
+        let mut grid = OctopusGrid::from_str(TEST_GRID_STRING).unwrap();
 
         assert_eq!(0, grid.advance());
-        assert_eq!(after_first_step, grid);
+        assert_eq!(
+            OctopusGrid {
+                energy_levels: [
+                    [6, 5, 9, 4, 2, 5, 4, 3, 3, 4],
+                    [3, 8, 5, 6, 9, 6, 5, 8, 2, 2],
+                    [6, 3, 7, 5, 6, 6, 7, 2, 8, 4],
+                    [7, 2, 5, 2, 4, 4, 7, 2, 5, 7],
+                    [7, 4, 6, 8, 4, 9, 6, 5, 8, 9],
+                    [5, 2, 7, 8, 6, 3, 5, 7, 5, 6],
+                    [3, 2, 8, 7, 9, 5, 2, 8, 3, 2],
+                    [7, 9, 9, 3, 9, 9, 2, 2, 4, 5],
+                    [5, 9, 5, 7, 9, 5, 9, 6, 6, 5],
+                    [6, 3, 9, 4, 8, 6, 2, 6, 3, 7],
+                ]
+            },
+            grid
+        );
 
         assert_eq!(35, grid.advance());
-        assert_eq!(after_second_step, grid);
+        assert_eq!(
+            OctopusGrid {
+                energy_levels: [
+                    [8, 8, 0, 7, 4, 7, 6, 5, 5, 5],
+                    [5, 0, 8, 9, 0, 8, 7, 0, 5, 4],
+                    [8, 5, 9, 7, 8, 8, 9, 6, 0, 8],
+                    [8, 4, 8, 5, 7, 6, 9, 6, 0, 0],
+                    [8, 7, 0, 0, 9, 0, 8, 8, 0, 0],
+                    [6, 6, 0, 0, 0, 8, 8, 9, 8, 9],
+                    [6, 8, 0, 0, 0, 0, 5, 9, 4, 3],
+                    [0, 0, 0, 0, 0, 0, 7, 4, 5, 6],
+                    [9, 0, 0, 0, 0, 0, 0, 8, 7, 6],
+                    [8, 7, 0, 0, 0, 0, 6, 8, 4, 8],
+                ]
+            },
+            grid
+        );
     }
 
     #[test]
-    fn test_advance_repeated() {
-        let mut grid = OctopusGrid::from_str(indoc! {"
-            5483143223
-            2745854711
-            5264556173
-            6141336146
-            6357385478
-            4167524645
-            2176841721
-            6882881134
-            4846848554
-            5283751526
-        "}).unwrap();
+    fn test_flashes_after_steps() {
+        assert_eq!(
+            1656,
+            OctopusGrid::from_str(TEST_GRID_STRING)
+                .unwrap()
+                .flashes_after_steps(100)
+        );
+    }
 
-        let mut flashes = 0;
-
-        for _ in 0..100 {
-            flashes += grid.advance();
-        }
-
-        assert_eq!(1656, flashes);
+    #[test]
+    fn test_steps_until_synchronization() {
+        assert_eq!(
+            195,
+            OctopusGrid::from_str(TEST_GRID_STRING)
+                .unwrap()
+                .steps_until_synchronization()
+        );
     }
 }

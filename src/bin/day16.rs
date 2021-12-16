2475ceb405eee8f5cb66e2115202b94e2584b38f
Jon Chambers <jon.chambers@gmail.com>
2021-12-16 18:37:18 -0500

    Add support for parsing literals

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,6 +1,7 @@
 use hex::FromHexError;
 use std::cmp::min;
 use std::{env, error};
+use crate::Packet::Operator;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -11,6 +12,72 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         Ok(())
     } else {
         Err("Usage: day16 INPUT_FILE_PATH".into())
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Header {
+    version: u8,
+    type_id: u8,
+}
+
+impl Header {
+    pub fn next_from_bit_stream(bit_stream: &mut BitStream) -> Self {
+        Header {
+            version: bit_stream.next_bits(3)[0],
+            type_id: bit_stream.next_bits(3)[0],
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Packet {
+    Literal {
+        header: Header,
+        value: u64,
+    },
+
+    Operator {
+        header: Header,
+        sub_packets: Vec<Packet>,
+    }
+}
+
+impl Packet {
+    const LITERAL_TYPE_ID: u8 = 4;
+    const LITERAL_HAS_MORE_BIT: u8 = 0b00010000;
+    const LITERAL_NIBBLE_MASK: u8 = 0b00001111;
+
+    pub fn next_from_bit_stream(bit_stream: &mut BitStream) -> Self {
+        let header = Header::next_from_bit_stream(bit_stream);
+
+        match header.type_id {
+            Packet::LITERAL_TYPE_ID => {
+                // Literal! Read five-bit chunks until we get a "last chunk" bit.
+                let mut value = 0u64;
+
+                loop {
+                    let next_nibble = bit_stream.next_bits(5)[0];
+
+                    value <<= 4;
+                    value |= (next_nibble & Packet::LITERAL_NIBBLE_MASK) as u64;
+
+                    if next_nibble & Packet::LITERAL_HAS_MORE_BIT == 0 {
+                        bit_stream.align_to_nibble();
+                        break;
+                    }
+                }
+
+                Packet::Literal { header, value }
+            },
+            _ => {
+                // Operator
+                Operator {
+                    header,
+                    sub_packets: vec![]
+                }
+            }
+        }
     }
 }
 
@@ -40,15 +107,7 @@ impl BitStream {
         BitStream { bytes, position: 0 }
     }
 
-    pub fn len(&self) -> usize {
-        self.bytes.len() * 8
-    }
-
-    pub fn next_bits(&mut self, n_bits: usize) -> Option<Vec<u8>> {
-        if self.position + n_bits >= self.len() {
-            return None;
-        }
-
+    pub fn next_bits(&mut self, n_bits: usize) -> Vec<u8> {
         let mut collected_bytes = Vec::new();
         let mut collected_bits = 0;
 
@@ -65,7 +124,7 @@ impl BitStream {
             collected_bytes.push(byte);
         }
 
-        Some(collected_bytes)
+        collected_bytes
     }
 
     fn next_bit(&mut self) -> u8 {
@@ -75,6 +134,10 @@ impl BitStream {
         self.position += 1;
 
         (self.bytes[byte_index] & BitStream::BIT_MASK[bit_offset]) >> (7 - bit_offset)
+    }
+
+    pub fn align_to_nibble(&mut self) {
+        self.position += (4 - (self.position % 4)) % 4;
     }
 }
 
@@ -107,10 +170,23 @@ mod test {
     fn test_next_bits() {
         let mut bit_stream = BitStream::new(vec![0xd2, 0xfe, 0x28]);
 
-        assert_eq!(Some(vec![0b110]), bit_stream.next_bits(3));
-        assert_eq!(Some(vec![0b100]), bit_stream.next_bits(3));
-        assert_eq!(Some(vec![0b10111]), bit_stream.next_bits(5));
-        assert_eq!(Some(vec![0b11110]), bit_stream.next_bits(5));
-        assert_eq!(Some(vec![0b00101]), bit_stream.next_bits(5));
+        assert_eq!(vec![0b110], bit_stream.next_bits(3));
+        assert_eq!(vec![0b100], bit_stream.next_bits(3));
+        assert_eq!(vec![0b10111], bit_stream.next_bits(5));
+        assert_eq!(vec![0b11110], bit_stream.next_bits(5));
+        assert_eq!(vec![0b00101], bit_stream.next_bits(5));
+    }
+
+    #[test]
+    fn test_literal_from_bit_stream() {
+        let mut bit_stream = BitStream::new(vec![0xd2, 0xfe, 0x28]);
+
+        assert_eq!(Packet::Literal {
+            header: Header {
+                version: 6,
+                type_id: 4,
+            },
+            value: 2021
+        }, Packet::next_from_bit_stream(&mut bit_stream));
     }
 }

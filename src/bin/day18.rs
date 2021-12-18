0c0b50acc3a283dc82921a10b500d8e4620f3fda
Jon Chambers <jon.chambers@gmail.com>
2021-12-18 17:09:36 -0500

    Add a solution for day 18, part 2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,4 +1,5 @@
 use std::{env, error, io};
+use std::cmp::max;
 use std::collections::VecDeque;
 use std::fmt::{Display, Formatter};
 use std::fs::File;
@@ -20,25 +21,44 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
         println!("Magnitude of sum: {}", magnitude_of_sum);
 
+        let numbers: Vec<SnailfishNumber> = io::BufReader::new(File::open(path)?).lines()
+            .filter_map(|line| line.ok())
+            .map(|line| SnailfishNumber::from_str(line.as_str()).unwrap())
+            .collect();
+
+        println!("Largest possible magnitude of any sum: {}", largest_pair_magnitude(&numbers));
+
         Ok(())
     } else {
         Err("Usage: day18 INPUT_FILE_PATH".into())
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+fn largest_pair_magnitude(numbers: &[SnailfishNumber]) -> u32 {
+    let mut max_magnitude = 0;
+
+    for i in 0..numbers.len() - 1 {
+        for j in i..numbers.len() {
+            max_magnitude = max(max_magnitude, (numbers[i].clone() + numbers[j].clone()).magnitude());
+            max_magnitude = max(max_magnitude, (numbers[j].clone() + numbers[i].clone()).magnitude());
+        }
+    }
+
+    max_magnitude
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 enum Element {
     Pair(Box<SnailfishNumber>),
     Literal(u32),
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct SnailfishNumber {
     left: Element,
     right: Element,
 }
 
-#[derive(Debug)]
 struct ExplodeState {
     zeroed_element: bool,
     left: Option<u32>,
@@ -447,5 +467,27 @@ mod test {
         for (string, expected) in test_cases {
             assert_eq!(expected, SnailfishNumber::from_str(string).unwrap().magnitude());
         }
+    }
+
+    #[test]
+    fn test_largest_pair_magnitude() {
+        let numbers_string: &str = indoc! {"
+            [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+            [[[5,[2,8]],4],[5,[[9,9],0]]]
+            [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+            [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+            [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+            [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+            [[[[5,4],[7,7]],8],[[8,3],8]]
+            [[9,3],[[9,9],[6,[4,9]]]]
+            [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+            [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+        "};
+
+        let numbers: Vec<SnailfishNumber> = numbers_string.lines()
+            .map(|line| SnailfishNumber::from_str(line).unwrap())
+            .collect();
+
+        assert_eq!(3993, largest_pair_magnitude(&numbers));
     }
 }
